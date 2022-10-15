c65919b71057e2142514d85d60b90969dafda182
danielpdaniel <artofjquinn@gmail.com>
2022-10-15 15:26:59 -0400

    commit tree 3a90f70800acc11d8967e12bbcd3a2a4c8a2fb2a

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -6,19 +6,46 @@ function SpicyFoodList() {
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
+    const newArray = [...foods, newFood]
+    setFoods(newArray)
     console.log(newFood);
   }
 
+  function handleLiClick(id){
+    const filteredFood = foods.map(food => {
+      if (food.id === id){
+        food.heatLevel+= 1;
+      }
+      return food
+    })
+    setFoods(filteredFood)
+  }
+
+  function handleSubmit(event){
+    const selectedCuisine = event.target.value;
+   
+    const matchingCuisines = foods.filter(food => food.cuisine === selectedCuisine)
+    setFoods(matchingCuisines)
+  }
+
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li key={food.id} onClick={() => handleLiClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
-  ));
+  ));  
 
   return (
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
       <ul>{foodList}</ul>
+      <select name="filter" onChange={handleSubmit}>
+        <option value="All">All</option>
+        <option value="American">American</option>
+        <option value="Sichuan">Sichuan</option>
+        <option value="Thai">Thai</option>
+        <option value="Mexican">Mexican</option>
+      </select>
+
     </div>
   );
 }
